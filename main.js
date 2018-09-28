400ca7832189cc7ae49e47a95ec2e1635536b021
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2018-09-28 19:58:17 +0300

    commit 5
    Lisättiin valmiin scriptin tulostus dokumentin alalaitaan. Muotoiltiin tulostusrakennetta. Kylän etsiminen on jätetty vielä kesken.

--- main.js
+++ main.js
@@ -9,25 +9,46 @@ document.getElementById("luo").onclick = function () {
     var perPage = document.getElementById("perPage").value;
     var area = document.getElementById("area").value;
     var village = document.getElementById("village").value;
+    var order = document.getElementById("order").value;
+    var orderedBy = document.getElementById("orderby").value;
     console.log("Palvelu: " + serviceSelected);
     console.log("Noston tyyppi: " + typeSelected);
     console.log("Nostojen määrä: " + perPage);
-    getVillage(village);
+    console.log("Kylää haettu: " + village);
+    //getVillage(village);
    
+   // console.log(getVillage(village));
+
     var reqURL = requestedURL();
+    console.log("*==*makeRestApiRequest*==*");
     makeRestApiRequest(reqURL, createHTML);
 
+
+    createJavaScript(reqURL); 
+ 
     function requestedURL(){
-      var requestURL = 'https://www.'+ serviceSelected +'.fi/wp-json/wp/v2/' + typeSelected + postPerPage(perPage) + getArea(area);
-      //console.log(requestURL);
+      var requestURL = 'https://www.'+ serviceSelected +'.fi/wp-json/wp/v2/' + typeSelected + postPerPage(perPage) + getArea(area) + orderBy(orderedBy) + ordered(order);
+      console.log(requestURL);
       return requestURL;
+    }
+
+    function orderBy(orderedBy){
+         return "&orderby=" + orderedBy; 
+    }
+
+    function ordered(order){
+        if (order=="asc"){
+            return "&order=asc";
+        } else {
+            return "&order=desc";
+        }
     }
 
     function postPerPage(perPage){
         if (perPage>100){
             perPage = 100;
-            console.log("Rajapinnasta ei voi nostaa enempää kuin 100 kohdetta.");
-            console.log("Asetetaan nostettujen kohteiden määräksi 100.");
+            // console.log("Rajapinnasta ei voi nostaa enempää kuin 100 kohdetta.");
+            // console.log("Asetetaan nostettujen kohteiden määräksi 100.");
         }
         var pages = '?per_page=' + perPage;
         return pages;
@@ -35,44 +56,63 @@ document.getElementById("luo").onclick = function () {
 
     function getArea(area){
         if (area==="none"){
-            console.log("Aluetta ei valittu");
+            //console.log("Aluetta ei valittu");
             var empty ='';
             return empty;
         } else {
             //do other stuff
-            console.log(area + " alue valittu");
+            //console.log(area + " alue valittu");
             var alue = '&tm_sote-alue=' + area;
             return alue;
         }
     }
 
+    //annetaan hautua
     function getVillage(village){
-        console.log("Haetaan kylää/kaupunginosaa " + village);
+        var a;
+        console.log("2. Haetaan kylää/kaupunginosaa " + village);
         var villageURL = 'https://www.'+ serviceSelected +'.fi/wp-json/wp/v2/tm_municipality?search=' + village;
-        console.log("Haetaan osoitteesta: " + villageURL);
-        var a = makeRestApiRequest(villageURL, parseVillageSearch);
-        console.log("Kylä/kaupunginosan id " + a);
+        console.log("3. Haetaan osoitteesta: " + villageURL);
 
-        // var kunta = '&tm_municipality' + kunta;
-        // return kunta; 
+        var villageRequest = new XMLHttpRequest();
+        villageRequest.open('GET', villageURL);
+        villageRequest.onload = function() {
+        if (villageRequest.status >= 200 && villageRequest.status < 400) {
+            var data = JSON.parse(villageRequest.responseText);
+            console.log(data);
+            a = parseVillageSearch(data);
+        } else {
+            console.log("We connected to the server, but it returned an error.");
+        }
+        };
+    
+        villageRequest.onerror = function() {
+            console.log("Connection error");
+        };
+    
+        villageRequest.send();
+        console.log("Asetetaan kylä url osoitteeseen: " + a);
+        if (a==undefined){
+
+        } else {
+            var municipality = "&tm_municipality="+a;
+            return municipality;
+        }
+
+        
+    console.log("*=====*getVillage function call completed*=====*");
     }
 
-    function makeRestApiRequest(requestURL, functionAfter){
+    function makeRestApiRequest(requestURL){
         //url to request, function to handle the json data
         var ourRequest = new XMLHttpRequest();
         ourRequest.open('GET', requestURL);
-        console.log("URL requested: " + requestURL)
+        //console.log("URL requested: " + requestURL)
         ourRequest.onload = function() {
         if (ourRequest.status >= 200 && ourRequest.status < 400) {
             var data = JSON.parse(ourRequest.responseText);
             console.log(data);
-            if (functionAfter !== undefined){
-                console.log("Function after called")
-                functionAfter(data);
-                return functionAfter(data);
-            } else {
-                console.log("Function after wast not called because function was not passed as parameter");
-            }
+            createHTML(data);
         } else {
             console.log("We connected to the server, but it returned an error.");
         }
@@ -85,25 +125,53 @@ document.getElementById("luo").onclick = function () {
         ourRequest.send();
     }
 
+    //unohda tämä tee muut ensin. 
     function parseVillageSearch(data){
         console.log("parseVillageSearch started");
+        var returnData;
             for (i=0;i<data.length;i++){
-                if (data[i].name == village){
-                    console.log("Kylä/kaupungin osa löytyi!")
-                    console.log("Kylän id on = "+ data[i].id);
-                    return data[i].id;
-                }
-            }
+                if (data[i].name.toUpperCase() == village.toUpperCase()){
+                    console.log("Kylä/kaupungin osa löytyi!");
+                    console.log("Kylän id on = "+ data[i].id);   
+                    returnData =  data[i].id;
+            } 
         }
+    }
 
     function createHTML(data){
-    console.log("Luodaan html nostoista...");
     var htmlString = '';
         for (i=0;i<data.length;i++){
     htmlString += '<h2><a href ='+ data[i].link +'>' + data[i].title.rendered +'</a>'+'</h2>';
+    htmlString += data[i].date;
     htmlString += data[i].content.rendered;
     }
     printHere.innerHTML = htmlString;
+    }
+
+
+    function createJavaScript(content){
+        var print = document.getElementById("js");
+        var code = "";
+        code +=  "<pre>"; 
+        code +=  "<code>"; 
+        code +=  "var printHere = document.getElementById('print');<br>";
+        code +=  "var ourRequest = new XMLHttpRequest();<br> ourRequest.open('GET', '"+content+"');<br>ourRequest.onload = function(){<br>if (ourRequest.status >= 200 && ourRequest.status < 400) {<br>var data = JSON.parse(ourRequest.responseText);<br>console.log(data);<br>createHTML(data);<br>} else {<br>console.log('We connected to the server, but it returned an error.');<br> }<br>};<br>ourRequest.onerror = function() {<br>console.log('Connection error');<br>};<br>ourRequest.send();";
+
+        code +=  "function ";
+        code +=  "createHTML(data){ <br>";
+        code +=  "var htmlString = '';<br>";
+        code +=  "for (i=0;i&ltdata.length;i++){<br>";
+        code +=  "htmlString += '<span><</span>h2><span><</span>a href ='+ data[i].link +'>' + data[i].title.rendered +'<span><</span>/a>'+'</h2>';<br>";
+        code +=  "htmlString += data[i].date;<br>";
+        code +=  "htmlString += data[i].content.rendered;<br>";
+        code +=  "}<br>";
+        code +=  "printHere.innerHTML = htmlString;<br>";
+        code +=  "}<br>";
+        code +=  "</code>";
+        code +=  "</pre>";
+
+        print.innerHTML = "";
+        print.innerHTML += code;
     }
 }
 
