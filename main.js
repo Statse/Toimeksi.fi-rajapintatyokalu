8fab83db72996389f266ed06a9ef51c612ae336e
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2018-11-09 13:40:25 +0200

    Updated print order and refactoring code

--- main.js
+++ main.js
@@ -12,22 +12,14 @@ document.getElementById("luo").onclick = function () {
     var kunta = document.getElementById("kunta").value;
     var order = document.getElementById("order").value;
     var orderedBy = document.getElementById("orderby").value;
-    // console.log("Palvelu: " + serviceSelected);
-    // console.log("Noston tyyppi: " + typeSelected);
-    // console.log("Nostojen määrä: " + perPage);
-   
 
     var reqURL = requestedURL();
-    console.log("*==*makeRestApiRequest*==*");
-
     makeRestApiRequest(reqURL, createHTML);
-
     createJavaScript(reqURL); 
 
    
  
     function requestedURL(){
-
       var requestURL = 'https://www.'+ serviceSelected +'.fi/wp-json/wp/v2/' + typeSelected + postPerPage(perPage) + getArea(area) + orderBy(orderedBy) + ordered(order) +  getKunta(kunta) + getVillage(village);
       console.log(requestURL);
       return requestURL;
@@ -49,8 +41,7 @@ document.getElementById("luo").onclick = function () {
     function postPerPage(perPage){
         if (perPage>100){
             perPage = 100;
-            // console.log("Rajapinnasta ei voi nostaa enempää kuin 100 kohdetta.");
-            // console.log("Asetetaan nostettujen kohteiden määräksi 100.");
+            alert("Rajapinnasta ei voi nostaa enempää kuin 100 kohdetta. Asetetaan määräksi 100.")
         }
         var pages = '?per_page=' + perPage;
         return pages;
@@ -58,12 +49,9 @@ document.getElementById("luo").onclick = function () {
 
     function getArea(area){
         if (area==="none"){
-            //console.log("Aluetta ei valittu");
             var empty ='';
             return empty;
         } else {
-            //do other stuff
-            //console.log(area + " alue valittu");
             var alue = '&tm_sote-alue=' + area;
             return alue;
         }
@@ -71,13 +59,13 @@ document.getElementById("luo").onclick = function () {
 
     function getKunta(kunta){
         var a;
-        var kuntaURL = 'https://www.'+ serviceSelected +'.fi/wp-json/wp/v2/tm_municipality?search=' + kunta;
+        var URL = 'https://www.'+ serviceSelected +'.fi/wp-json/wp/v2/tm_municipality?search=' + kunta;
 
-        var kuntaRequest = new XMLHttpRequest();
-        kuntaRequest.open('GET', kuntaURL, false);
-        kuntaRequest.onload = function() {
-        if (kuntaRequest.status >= 200 && kuntaRequest.status < 400) {
-            var data = JSON.parse(kuntaRequest.responseText);
+        var theRequest = new XMLHttpRequest();
+        theRequest.open('GET', URL, false);
+        theRequest.onload = function() {
+        if (theRequest.status >= 200 && theRequest.status < 400) {
+            var data = JSON.parse(theRequest.responseText);
             console.log(data);
             a = parseSearch(data, kunta);
         } else {
@@ -85,34 +73,34 @@ document.getElementById("luo").onclick = function () {
         }
         };
     
-        kuntaRequest.onerror = function() {
+        theRequest.onerror = function() {
             console.log("Connection error");
+            alert("Connection error.")
         };
     
-        kuntaRequest.send();
-        if (kuntaRequest.readyState == 4){
+        theRequest.send();  
+        
+        if (theRequest.readyState == 4){
             if (a==undefined){
-                console.log("Returnin undefined as kunta");
                 return '';
             } else {
                 console.log("Asetetaan kylä url osoitteeseen: " + a);
                 var municipality = "&tm_municipality="+a;
-                console.log("*=====*getKunta function call completed*=====*");
                 return municipality;
             }
         }
     }
 
     
-    function getVillage(kunta){ //basically same code as getKunta so i didnt change the terminology
+    function getVillage(kunta){ 
         var a;
-        var kuntaURL = 'https://www.'+ serviceSelected +'.fi/wp-json/wp/v2/tm_village?search=' + kunta;
+        var URL = 'https://www.'+ serviceSelected +'.fi/wp-json/wp/v2/tm_village?search=' + kunta;
 
-        var kuntaRequest = new XMLHttpRequest();
-        kuntaRequest.open('GET', kuntaURL, false);
-        kuntaRequest.onload = function() {
-        if (kuntaRequest.status >= 200 && kuntaRequest.status < 400) {
-            var data = JSON.parse(kuntaRequest.responseText);
+        var theRequest = new XMLHttpRequest();
+        theRequest.open('GET', URL, false);
+        theRequest.onload = function() {
+        if (theRequest.status >= 200 && theRequest.status < 400) {
+            var data = JSON.parse(theRequest.responseText);
             console.log(data);
             a = parseSearch(data, kunta);
         } else {
@@ -120,20 +108,18 @@ document.getElementById("luo").onclick = function () {
         }
         };
     
-        kuntaRequest.onerror = function() {
+        theRequest.onerror = function() {
             console.log("Connection error");
         };
     
-        kuntaRequest.send();
-        if (kuntaRequest.readyState == 4){
+        theRequest.send();  
+        
+        if (theRequest.readyState == 4){
             if (a==undefined){
-              //  console.log("Returnin undefined as kunta");
                 return '';
             } else {
-                //console.log("Asetetaan kylä url osoitteeseen: " + a);
-                var municipality = "&tm_village="+a;
-               // console.log("*=====*getKunta function call completed*=====*");
-                return municipality;
+                var kyla = "&tm_village="+a;
+                return kyla;
             }
         }
     }
@@ -143,7 +129,6 @@ document.getElementById("luo").onclick = function () {
         //url to request, function to handle the json data
         var ourRequest = new XMLHttpRequest();
         ourRequest.open('GET', requestURL);
-        //console.log("URL requested: " + requestURL)
         ourRequest.onload = function() {
         if (ourRequest.status >= 200 && ourRequest.status < 400) {
             var data = JSON.parse(ourRequest.responseText);
@@ -161,14 +146,10 @@ document.getElementById("luo").onclick = function () {
         ourRequest.send();
     }
 
-    //unohda tämä tee muut ensin. 
-    function parseSearch(data, passedValue){
-        console.log("parseSearch started");
+    function parseSearch(data, passedValue){//search for area
         var returnData;
             for (i=0;i<data.length;i++){
                 if (data[i].name.toUpperCase() == passedValue.toUpperCase()){
-                    console.log("Kylä/kaupungin osa löytyi!");
-                    console.log("Kylän id on = "+ data[i].id);   
                     returnData =  data[i].id;
             } 
         }
@@ -193,7 +174,6 @@ document.getElementById("luo").onclick = function () {
         code +=  "<code>"; 
         code +=  "var printHere = document.getElementById('print');<br>";
         code +=  "var ourRequest = new XMLHttpRequest();<br> ourRequest.open('GET', '"+content+"');<br>ourRequest.onload = function(){<br>if (ourRequest.status >= 200 && ourRequest.status < 400) {<br>var data = JSON.parse(ourRequest.responseText);<br>console.log(data);<br>createHTML(data);<br>} else {<br>console.log('We connected to the server, but it returned an error.');<br> }<br>};<br>ourRequest.onerror = function() {<br>console.log('Connection error');<br>};<br>ourRequest.send();";
-
         code +=  "function ";
         code +=  "createHTML(data){ <br>";
         code +=  "var htmlString = '';<br>";
